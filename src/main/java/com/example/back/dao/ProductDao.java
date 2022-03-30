e7090ce569abe473b49fdf3a381c9b28971c8e60
MAURO JEAN-FRANCOIS <jeanfrancois.mauro@minfin.fed.be>
2022-03-30 11:22:08 +0200

    first commit

---
+++ src/main/java/com/example/back/dao/ProductDao.java
@@ -0,0 +1,11 @@
+package com.example.back.dao;
+
+
+import com.example.back.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProductDao extends JpaRepository<Product, Integer>{
+}
---
+++ src/main/java/com/example/back/web/controller/ProductController.java
@@ -0,0 +1,57 @@
+package com.example.back.web.controller;
+
+
+
+import com.example.back.dao.ProductDao;
+import com.example.back.model.Product;
+import com.example.back.web.exceptions.ProductNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductDao productDao;
+
+    @CrossOrigin(origins = "http://localhost:4200", maxAge = 60)
+    @GetMapping(value = "/products")
+    public List<Product> findAll() {
+
+        List<Product> products = productDao.findAll();
+
+        if (products.isEmpty()) throw new ProductNotFoundException("Aucun produit n'est disponible à la vente");
+
+        return products;
+
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200", maxAge = 60)
+    @GetMapping(value = "/products/{id}")
+    public Optional<Product> findById(@PathVariable int id) {
+
+        Optional<Product> product = productDao.findById(id);
+
+        if (!product.isPresent())
+            throw new ProductNotFoundException("Le produit correspondant à l'id " + id + " n'existe pas");
+
+        return product;
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200", maxAge = 60)
+    @PostMapping(value = "/products")
+    public ResponseEntity<Product> add(@RequestBody Product product){
+
+        Product p = productDao.save(product);
+
+        //if(p == null) throw new ImpossibleAjouterCommandeException("Impossible d'ajouter cette commande");
+
+        return new ResponseEntity<Product>(p, HttpStatus.CREATED);
+    }
+}
+
